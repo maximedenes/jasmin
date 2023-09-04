5f9de4a5d8ec592a4e1eca8d2405ef47c75c6cda
Maxime Dénès <maxime.denes@inria.fr>
2023-09-04 21:22:21 +0200

    wip

--- default.nix
+++ default.nix
@@ -49,7 +49,7 @@ let ecDeps = ecRef != ""; in
 
 stdenv.mkDerivation {
   name = "jasmin-0";
-  src = null;
+  src = ./.;
   buildInputs = []
     ++ optionals coqDeps [ coqPackages.coq mathcomp-word ]
     ++ optionals testDeps ([ curl.bin oP.apron.out libllvm ] ++ (with python3Packages; [ python pyyaml ]))
